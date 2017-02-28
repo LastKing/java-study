df3bbb2730339805db793badafab97bea267d40f
李太新 <toonew@danwi.me>
2017-02-28 11:33:02 +0800

    枚举类 学习，基本了解

---
+++ src/com/base/enums/TestColor.java
@@ -0,0 +1,42 @@
+package com.base.enums;
+
+import com.sun.prism.j2d.paint.MultipleGradientPaint;
+
+/**
+ * 枚举实例
+ * Created by Rain on 2017/2/28.
+ */
+public class TestColor {
+
+    public static void main(String[] args) {
+        System.out.println(isRed(Color.BLANK));
+        System.out.println(isRed(Color.RED));
+
+        showColor(Color.BLANK);
+
+        System.out.print(Color.BLANK.getName());
+        System.out.println(Color.BLANK.getIndex());
+
+        for (Color color : Color.values()) {
+            System.out.println(color + " name: " + color.getName() + " index: " + color.getIndex());
+        }
+    }
+
+    static boolean isRed(Color color) {
+        return Color.RED.equals(color);
+    }
+
+    static void showColor(Color color) {
+        switch (color) {
+            case BLANK:
+                System.out.println(color);
+                break;
+            case RED:
+                System.out.println(color);
+                break;
+            default:
+                System.out.println(color);
+                break;
+        }
+    }
+}
