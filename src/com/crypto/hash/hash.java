ddc4a6fd06c5815474bc8d76a2e119ec72e606f0
litaixin <litaixin01@163.com>
2018-02-01 22:45:15 +0800

    修改案例

--- src/com/crypto/hash/hash.java
+++ src/com/crypto/hash/hash.java
@@ -7,22 +7,23 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * hash md5
- * <p>
  * Created by toonew on 2017/4/20.
  */
 public class hash {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         String str = getMD5("hello 世界");
+        String str2 = getMD5("hello 世界2");
         System.out.println(str);
+        System.out.println(str2);
     }
 
-    public static String getMD5(String str) throws NoSuchAlgorithmException {
+    private static String getMD5(String str) throws NoSuchAlgorithmException {
         MessageDigest md5 = null;
         md5 = MessageDigest.getInstance("MD5");
         try {
 //            md5.update(str. getBytes("UTF-8"));
-            md5.update(str. getBytes("iso-8859-1"));
+            md5.update(str.getBytes("iso-8859-1"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
