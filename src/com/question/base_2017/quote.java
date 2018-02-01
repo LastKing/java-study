63979da1dd64fdf1471529ec71506c1892d6d538
litaixin <litaixin01@163.com>
2018-02-02 00:33:24 +0800

    不能理解的引用队列

--- src/com/question/base_2017/quote.java
+++ src/com/question/base_2017/quote.java
@@ -12,19 +12,22 @@ public class quote {
         //1. 强引用
         Object obj = new Object();
 
-        System.out.println("===========强引用============");
+        System.out.println("===========软引用============");
         //2. 软银用
         soft();
 
-        System.out.println("===========软引用============");
-        //3. 软引用
+        System.out.println("===========弱引用============");
+        //3. 弱引用
         weak();
+
+        //4. 虚引用
+        xu();
     }
 
     /**
      * 只有当内存不够的时候，才回收这类内存，因此在内存足够的时候，
-     * 它们通常不被回收  无论是否发送GC,执行结果都是:
-     * java.lang.Object@f9f9d8 * null * java.lang.Object@f9f9d8 * null *
+     * 它们通常不被回收 无论是否发送GC,执行结果都是:
+     * java.lang.Object@f9f9d8 * null * java.lang.Object@f9f9d8 * null
      * 可以看到:只有发送了GC,将对于从内存中释放的时候,JVM才会将reference加入引用队列
      */
     private static void soft() throws Exception {
@@ -67,5 +70,17 @@ public class quote {
         System.out.println(refQueue.poll());
         System.out.println(refQueue.poll());//这里一定是null,因为已经从队列中移除
         System.out.println(refQueue.remove());
+    }
+
+    /**
+     * 当GC一但发现了虚引用对象，将会将PhantomReference对象插入ReferenceQueue队列.
+     * 而此时PhantomReference所指向的对象并没有被GC回收，而是要等到ReferenceQueue被你真正的处理后才会被回收.
+     * 这里特别需要注意：当JVM将虚引用插入到引用队列的时候，虚引用执行的对象内存还是存在的。
+     * 但是PhantomReference并没有暴露API返回对象。所以如果我想做清理工作，
+     * 需要继承PhantomReference类，以便访问它指向的对象。
+     * 如NIO直接内存的自动回收，就使用到了sun.misc.Cleaner
+     */
+    private static void xu() {
+        System.out.println("虚引用");
     }
 }
