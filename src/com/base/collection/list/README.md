21a43cad647a636070350faacb1b790c0a0bb409
李太新 <litaixin@donews.com>
2018-03-19 12:59:46 +0800

    增加ArrayList 一些面试题

---
+++ src/com/base/collection/list/README.md
@@ -0,0 +1,12 @@
+# list 相关学习
+
+主要三种
+ArrayList Vector LinkList
+
+## ArrayList
+1. [ArrayList几问](https://zhuanlan.zhihu.com/p/34301705?utm_source=wechat_session&utm_medium=social)
+
+## Vector
+
+
+## LinkList
--- src/com/base/collection/list/Test1.java
+++ src/com/base/collection/list/Test1.java
@@ -1,9 +1,6 @@
 package com.base.collection.list;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * List
@@ -11,11 +8,12 @@ import java.util.Vector;
  */
 public class Test1 {
     public static void main(String[] args) {
-        //ArrayList是非线程安全的，效率高
+        //ArrayList是非线程安全的，效率高   （顺序存储）
         List list = new ArrayList();
 
         //Vector   是synchronized实现的线程安全，效率低 （以上两者都是 数组的动态扩容实现的）
         List list2 = new Vector();
+        List list4 = Collections.synchronizedList(list);
 
         //LinkedList：底层基于链表实现，链表内存是散乱的，
         // 每一个元素存储本身内存地址的同时还存储下一个元素的地址。链表增删快，查找慢
