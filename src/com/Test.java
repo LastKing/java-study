2fd79e6dda058ea80e29e27841b1c050a5083c6c
李太新 <litaixin@donews.com>
2018-03-21 10:42:34 +0800

    补充文字解释

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,7 +4,10 @@ package com;
  * Created by toonew on 2017/3/28.
  */
 public class Test {
+
     public static void main(String[] args) {
+        
 
     }
+
 }
--- src/com/base/collection/list/Test1.java
+++ src/com/base/collection/list/Test1.java
@@ -8,15 +8,18 @@ import java.util.*;
  */
 public class Test1 {
     public static void main(String[] args) {
-        //ArrayList是非线程安全的，效率高   （顺序存储）
+        //ArrayList ：是非线程安全的，效率高   （顺序存储）
         List list = new ArrayList();
 
-        //Vector   是synchronized实现的线程安全，效率低 （以上两者都是 数组的动态扩容实现的）
+        //Vector ：是synchronized实现的线程安全，效率低 （以上两者都是 数组的动态扩容实现的）
         List list2 = new Vector();
         List list4 = Collections.synchronizedList(list);
 
-        //LinkedList：底层基于链表实现，链表内存是散乱的，
+        //LinkedList ：底层基于链表实现，链表内存是散乱的，
         // 每一个元素存储本身内存地址的同时还存储下一个元素的地址。链表增删快，查找慢
         List list3 = new LinkedList();
+
+        //Stack ：继承自Vector
+        List list5 = new Stack();
     }
 }
